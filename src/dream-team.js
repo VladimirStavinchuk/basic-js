5baf252f177dd4861d05ab8d61a28bafdeff964e
VladimirStavinchuk <sun10@mail.ru>
2022-05-01 18:27:54 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require ('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,11 +13,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam (members) {
+  if (!Array.isArray (members)) {
+    return false;
+  }
+
+  return members
+    .reduce ((dreamTeam, item) => {
+      return typeof item === 'string'
+        ? dreamTeam + item.match (/[a-zA-Z]/)[0].toUpperCase ()
+        : dreamTeam;
+    }, '')
+    .split ('')
+    .sort ()
+    .join ('');
+
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
+
+// console.log (createDreamTeam (['sadsa', 1111, 'zzz']));
+
+// console.log (
+//   createDreamTeam ([
+//     '   William Alston ',
+//     ' Paul Benacerraf',
+//     '  Ross Cameron',
+//     '       Gilles Deleuze',
+//     '  Arda Denkel ',
+//     '  Michael Devitt',
+//     '  Kit Fine',
+//     ' Nelson Goodman',
+//     'David Kolb',
+//     '   Saul Kripke',
+//     '  Trenton Merricks',
+//     '  Jay Rosenberg',
+//   ])
+// );
