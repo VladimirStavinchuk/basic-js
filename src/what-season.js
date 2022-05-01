4ce61e132a8db6d17b6a0ac0b33645c84ba8e3a3
VladimirStavinchuk <sun10@mail.ru>
2022-05-01 22:57:52 +0300

    feat: add what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require ('../extensions/index.js');
 
-/**
- * Extract season from given date and expose the enemy scout!
- * 
- * @param {Date | FakeDate} date real or fake date
- * @returns {String} time of the year
- * 
- * @example
- * 
- * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
- */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason (date) {
+  if (!date) return 'Unable to determine the time of year!';
+
+  if (typeof date.getMonth != 'function' || date.length === 0) {
+    throw new Error ('Invalid date!');
+  }
+  if (Object.getOwnPropertyNames (date).length > 0) {
+    throw new Error ('Invalid date!');
+  }
+
+  let month = date.getMonth ();
+  let season = '';
+
+  let seasons = {
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    autumn: [8, 9, 10],
+    winter: [11, 0, 1],
+  };
+
+  for (const key in seasons) {
+    // console.log (`${month} ${key}: ${seasons[key]}`);
+    if (seasons[key].includes (month)) {
+      season = key;
+    }
+  }
+
+  return season;
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
+
+// сначала нужно проверить что объектный toString возвращает [object Date]
+// а затем нужно проверить, что в этом объекте нет собственных методов getMonth, я использовал Object.keys(date).length
+
+// В "обычном" моменте с подвохом создается объект без методов, который имитирует Date. А в "особом" моменте с подвохом ему еще добавляются псевдометоды, которые есть у объекта Date(getMonth, getFullYear, getDate, getHours, getMinutes, getSeconds, getMilliseconds, getDay)
+
+// Denis Mythnik(@TheMrCrowley) — 11.09.2021
+// попробуй при помощи конструкции try...catch вызвать у аргумента метод, который есть только у объекта Date
+// Ilya_S — 11.09.2021
+// Либо без обработки ошибки принять аргументы как Object.hasOwnPropertyNames(arguments).length > 0 и выкинуть саму ошибку
